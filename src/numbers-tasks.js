52923c57e2262fb85db46715671ad2c4bbcf065a
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-12-23 16:58:43 +0100

    feat: realization isPrime function (Mon, Dec 23, 2024 16:58:27 PM)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -208,8 +208,25 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (!n || n <= 1) {
+    return false;
+  }
+
+  if (n === 2 || n === 3) {
+    return true;
+  }
+
+  if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+
+  for (let i = 5; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0 || n % (n + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
