185ec406a3412441e86d1cc27ca3a78ef6a62c92
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-12-24 12:18:32 +0100

    feat: realization toNumber function (Tue, Dec 24, 2024 12:18:05 PM)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -244,8 +244,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return value instanceof Number || +value ? +value : def;
 }
 
 /**
